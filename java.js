0ead51070377bc73e54ac25c64a5aeb73f19b2f8
ShahyanShah <shahyanshah798@gmail.com>
2024-08-30 21:00:39 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,73 @@
+let firsstname='shahyan'
+console.log('shahyan');
+let intrestrate=0.1;
+intrestrate=1
+console.log(intrestrate);
+
+
+//word vs keyword
+//for=keyword
+//chahcha=word
+
+//var const let
+var man="king";
+var women="queen";
+console.log(man  +women)
+
+//hoisting=varaible and function are hoisted which means declaration is moved at top
+
+//primities and refernce
+//primities=string,boolean,number etc
+//refernce=(){}[]
+var a=[12,13,14];
+var b=a;
+b.pop()
+
+//conditinols 
+//if else elseif
+//if(true){
+
+//}
+//else{
+
+//}
+
+//loop for,while
+for(var i=25;i<51;i++){
+    console.log(i);
+}
+var ab=2;
+while(ab<10){
+    ab++;
+    console.log(ab);
+}
+
+//functions
+
+function hi(){
+    console.log("hi");
+}
+hi();
+
+//Array
+var arra=[1,2,3,4,5];
+
+//push pop shift unshift
+arra.push()
+arra.pop()
+arra.shift()
+arra.unshift()
+arra.splice(2,1)
+
+//object
+//1 blank obj
+var about={};
+//2 filled obj
+var about ={
+    age:24,
+    brand:"gucci",
+    
+}
+about.gucci="nike"
+
+    
